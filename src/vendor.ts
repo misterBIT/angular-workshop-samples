90c98657b5c2358806158e6be246247e7238d656
Nadav Sinai <ns@nadavsinai.com>
2016-05-28 21:56:03 +0300

    add long stacktrace

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,6 +1,9 @@
 import 'core-js/es6';
+
 import 'core-js/es7/reflect';
+Error['stackTraceLimit'] = Infinity;
 require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import '@angular/platform-browser'
