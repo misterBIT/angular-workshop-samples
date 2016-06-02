6260ce612017f09153982093845be480b0d49335
nadavsinai <ns@nadavsinai.com>
2016-06-02 15:11:57 +0300

    some additions done in the class 31/05/2016

--- src/samples/017-coloring/coloring-input.directive.ts
+++ src/samples/017-coloring/coloring-input.directive.ts
@@ -1,20 +1,27 @@
-import {Directive, ElementRef, Input} from '@angular/core';
+import {Directive, ElementRef, Input,PLATFORM_DIRECTIVES} from '@angular/core';
 @Directive({
-    selector: '[coloring-input]'
+	selector: '[coloring-input]'
 })
 export class ColoringInputDirective {
-    constructor(el: ElementRef) {
-        el.nativeElement.addEventListener('keyup', ()=> {
-            el.nativeElement.style.backgroundColor = this.getRandomColor();
-        });
+	@Input("coloring-input") letters;
 
-    }
-    private getRandomColor() {
-        var letters = '0123456789ABCDEF'.split('');
-        var color = '#';
-        for (var i = 0; i < 6; i++ ) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
+	constructor(el:ElementRef) {
+		el.nativeElement.addEventListener('keyup', ()=> {
+			el.nativeElement.style.backgroundColor = this.getRandomColor();
+		});
+
+	}
+	
+	ngOnInit(){
+		console.log(this.letters); // just to demo we can get input of a string withouut binding and that the [] does not alter the selector matching
+	}
+
+	private getRandomColor() {
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		for (var i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
 }
