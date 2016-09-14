5007c284c887341301edf40656f5d4e2c7fd695b
Nadav Sinai <ns@nadavsinai.com>
2016-09-14 12:37:51 +0300

    little example to demo @optional and viewProviders

--- src/day3/055-Tabs-ContentProjection/tab.component.ts
+++ src/day3/055-Tabs-ContentProjection/tab.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, Optional} from '@angular/core';
 import {TabsComponent} from './tabs.component';
+import {Test} from "./test.service";
 
 @Component({
 	selector: 'tab',
@@ -14,7 +15,8 @@ export class TabComponent {
 	@Input() tabTitle;
 	active;
 
-	constructor(tabs:TabsComponent) {
+	constructor(tabs:TabsComponent,@Optional() test:Test) {
+		console.log(test);
 		tabs.addTab(this);
 	}
 
--- src/day3/055-Tabs-ContentProjection/tabs.component.ts
+++ src/day3/055-Tabs-ContentProjection/tabs.component.ts
@@ -1,29 +1,33 @@
 import {Component} from '@angular/core';
 import {TabComponent} from './tab.component';
+import {Test} from "./test.service";
 @Component({
-    selector: 'tabs',
-    // styles: [`li.active {color:orange}`],
-    template: `
+	selector: 'tabs',
+	// styles: [`li.active {color:orange}`],
+	viewProviders: [Test],
+	template: `
     <ul class="nav nav-tabs">
         <li *ngFor="let tab of tabs" (click)="selectTab(tab)" [class.active]="tab.active">
             <a href="#">{{tab.tabTitle}}</a>
         </li> 
+        <tab></tab>
     </ul>
     <ng-content></ng-content>
     `
 })
 
-export class TabsComponent{
-    tabs:TabComponent[] = [];
-    
-    addTab(tab:TabComponent) {
-        if (this.tabs.length === 0) tab.active = true;
-        this.tabs.push(tab);
-    }  
-    selectTab(tab:TabComponent) {
-        this.tabs.forEach((tab) => {
-            tab.active = false;
-        });
-        tab.active = true
-    }
+export class TabsComponent {
+	tabs: TabComponent[] = [];
+
+	addTab(tab: TabComponent) {
+		if (this.tabs.length === 0) tab.active = true;
+		this.tabs.push(tab);
+	}
+
+	selectTab(tab: TabComponent) {
+		this.tabs.forEach((tab) => {
+			tab.active = false;
+		});
+		tab.active = true
+	}
 }
