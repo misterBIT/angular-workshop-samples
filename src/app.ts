cc71e1a84e2aaa1410b7b902b5167e8a7af1a9d3
nadavsinai <ns@nadavsinai.com>
2016-09-25 23:00:34 +0300

    fix all forms examples

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@
 // import "./day1/000-Tries/main";
 // import "./day1/010-counterInputOutput/main";
 // import "./day1/012-DisplayAndEvents/main";
-//import "./day1/020-BuiltinDirectives/main";
+// import "./day1/020-BuiltinDirectives/main";
 //import "./day1/025-Pipes/main";
 //import "./day1/070-DI-Cars/main";
 // import './day1/pets/main';
@@ -15,40 +15,38 @@
  Day2
  *********/
 // import "./day2/lifecycle/main";
-// import "./day2/040-MonsterForm/main";
+//import "./day2/040-MonsterForm/main";
 // import "./day2/forms-live-coding/main.ts";
 // import "./day2/forms-live-coding-template-driven/main.ts";
 // import './day2/forms-live-coding-model-driven/main.ts';
-// import "./day2/041-ProductForms/main";
-// import "./day2/042-FormsCustomValidations/main";
-// import "./day2/043-FormsAsynchValidator/main";
-// import "./day2/dynamicForms/main.ts";
-// import "./day2/custom-form-controls/main";
-// import "./day2/FormValidationsSignup-PassRetypeChallange/main";
-// import './day2/NestedModelDrivenForms/main.ts'
-// import './day2/VariousFormControls/main.ts'
+// import "./day2/forms-live-coding/main.ts";
+//import "./day2/041-ProductForms/main";
+//import "./day2/042-FormsCustomValidations/main";
+//import "./day2/043-FormsAsynchValidator/main";
+//import "./day2/dynamicForms/main.ts";
+//import "./day2/custom-form-controls/main";
+//import "./day2/FormValidationsSignup-PassRetypeChallange/main";
+//import './day2/NestedModelDrivenForms/main.ts'
+//import './day2/VariousFormControls/main.ts'
 // import "./day2/routing-monsters/main.ts";
 /**********
  Day3
  *********/
-//import "./day3/010-SimpleCustomDirective/main";
+// import "./day3/010-SimpleCustomDirective/main";
 //import "./day3/015-refToChildElement/main";
 //import "./day3/016-refToContentChildren/main";
-//import "./day3/017-coloring/main";
+// import "./day3/017-coloring/main";
 //import "./day3/050-Collapse-ContentProjection/main";
 // import "./day3/055-Tabs-ContentProjection/main";
 // import "./day3/056-Tabs-ContentProjection2/main";
-//import "./day3/056-Tabs-ContentProjection3-addTab/main";
+// import "./day3/056-Tabs-ContentProjection3-addTab/main";
 //import "./day3/080-ngForTrackBy/main";
-// import "./day3/081-InjectingPipe/main";
-//import "./day3/modal-component/main";
-// import "./day3/rxjs/wikipediaSearch/main.ts"
 
 /**********
  Day4
  *********/
 //import "./day4/CustomElementInterop/main";
-import "./day4/monstersAndChat/main";
+import './day4/monstersAndChat/main';
 // import "./day4/animate-samples/demo1/main";
 // import "./day4/animate-samples/demo-states/main";
 // import "./day4/animate-samples/demo-scene/main";
