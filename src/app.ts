d21c07d75acbb99a7cbd59f7105f4f88c29a761e
nadavsinai <ns@nadavsinai.com>
2016-12-08 17:50:38 +0200

    fix app,ts missing comments

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,58 @@
-import './day5/redux_pattern_logout_timer/main';
+import './day2/000-Tries/main';
+// import "./day2/010-counterInputOutput/main";
+// import "./day2/012-DisplayAndEvents/main";
+// import "./day2/020-BuiltinDirectives/main";
+// import "./day2/025-Pipes/main";
+// import './day2/pets/main';
+// import './day2/pets-starter/main';
+
+/**********
+ Day3
+ *********/
+// import "./day3/070-DI-Cars/main";
+// import "./day3/lifecycle/main";
+// import "./day3/010-CustomDirectives/main";
+// import "./day3/015-refToViewChildren/main";
+// import "./day3/016-refToContentChildren/main";
+// import "./day3/050-Collapse-ContentProjection/main";
+// import "./day3/055-Tabs-ContentProjection/main";
+// import "./day3/056-Tabs-ContentProjection2/main";
+// import "./day3/056-Tabs-ContentProjection3-addTab/main";
+// import "./day3/080-ngForTrackBy/main";
+// import "./day3/modal-component/main";
+// import "./day3/090-ObservableDataServices/main";
+//
+/**********
+ Day4
+ *********/
+// import "./day4/040-MonsterForm/main";
+// import "./day4/forms-live-coding-template-driven/main.ts";
+// import './day4/forms-live-coding-model-driven/main.ts';
+// import "./day4/forms-live-coding/main.ts";
+// import "./day4/041-ProductForms/main";
+// import "./day4/042-FormsCustomValidations/main";
+// import "./day4/043-FormsAsynchValidator/main";
+// import "./day4/dynamicForms/main.ts";
+// import "./day4/custom-form-controls/main";
+// import "./day4/FormValidationsSignup-PassRetypeChallange/main";
+// import './day4/NestedModelDrivenForms/main.ts'
+// import './day4/VariousFormControls/main.ts'
+// import "./day4/routing-monsters/main.ts";
+
+
+
+/**********
+ Day5
+ *********/
+// import './day5/setDocmentTitle/main.ts'
+// import "./day5/CustomElementInterop/main";
+// import './day5/monstersAndChat/main';
+// import "./day5/animate-samples/demo1/main";
+// import "./day5/animate-samples/demo-states/main";
+// import "./day5/animate-samples/demo-scene/main";
+
+// import './day5/notifications_panel_animations/main';
+// import './day5/redux_pattern_logout_timer/main';
 
 /**********
  Day6
